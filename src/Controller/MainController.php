7bb23555fdc1983bdc5c74cbf8a1d978a12990fe
nirodriguez <nirodriguez@salud.gob.sv>
2019-10-21 11:59:36 -0600

    se integra symfony

---
+++ src/Controller/MainController.php
@@ -0,0 +1,40 @@
+<?php
+namespace App\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpKernel\Kernel;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class MainController extends Controller
+{
+
+    /**
+     * @Route("/", name="index")
+     */
+    public function index()
+    {
+
+        $response = $this->render('main/home.html.twig');
+                return $response;
+    }
+    /**
+     * @Route("crearcv", name="crearcv")
+     */
+    public function crearcv()
+    {
+
+        $response = $this->render('utilidades/crearcv.html.twig');
+        return $response;
+    }
+    /**
+     * @Route("mihistoria", name="mihistoria")
+     */
+    public function miHistoria()
+    {
+
+        $response = $this->render('historia/mihistoria.html.twig');
+        return $response;
+    }
+}
